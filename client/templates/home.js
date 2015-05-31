8ddb3bdd666d71aa6318673ebd005a0df2e14d8f
Adi De Masi <adi@panter.ch>
2015-05-31 16:38:43 +0200

    experimenting, step one

---
+++ client/templates/home.js
@@ -0,0 +1,23 @@
+Template.players.events({
+    'submit #players': function (e) {
+        e.preventDefault();
+        var username = e.target.username.value;
+        PlayersList.insert({name: username});
+        Session.set({username: username});
+    },
+    'submit #features': function (e) {
+        e.preventDefault();
+        FeaturesList.insert({name: e.target.feature.value});
+    }
+});
+Template.players.helpers({
+    'players': function () {
+        return PlayersList.find();
+    },
+    'features': function () {
+        return FeaturesList.find();
+    },
+   'usernameEntered': function () {
+       return Session.get('username') === undefined ? 'visible' : 'invisible';
+   } 
+});
---
+++ collections.js
@@ -0,0 +1,2 @@
+PlayersList = new Mongo.Collection('players');
+FeaturesList = new Mongo.Collection('features');
