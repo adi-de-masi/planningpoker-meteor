ebc9ebf0ec61ca7f7c4dd98882604292e3cf19c0
Adi De Masi <adi@panter.ch>
2015-05-31 18:39:43 +0200

    added primitive card selection

--- client/templates/vote.js
+++ client/templates/vote.js
@@ -1,4 +1,4 @@
-Template.vote.events({
+Template.card.events({
   'submit #vote': function (e) {
 
     var form = e.target,
@@ -8,5 +8,26 @@ Template.vote.events({
     e.preventDefault();
 
     VotesList.insert({username:currentUser, points:points});
+  },
+  'click': function (e,template) {
+      Session.set({'choice': template.data.value});
   }
+});
+Template.card.helpers({
+    'isSelected': function() {
+        return Session.get('choice') === this.value;
+    }
+});
+Template.vote.helpers({
+    'fibonaccis': function () {
+        return [{value: 1},
+            {value: 3},
+            {value: 5},
+            {value: 8},
+            {value: 13},
+            {value: 21}];
+    },
+    'choice': function () {
+        return Session.get('choice');
+    }
 });
