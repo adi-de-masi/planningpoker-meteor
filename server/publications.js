29c196817f57fa6048fed9edb794fd88ef5519e9
Raffael Schmid <ras@panter.ch>
2015-06-01 15:21:54 +0200

    fixes

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,8 @@
-Meteor.publish("client", function(userId) {
-  this.onStop(function(){
-    PlayersList.remove(userId);
+Meteor.publish("room", function (username, roomId) {
+
+  PlayersList.upsert({username:username, roomId:roomId}, {$set:{username:username, roomId:roomId}});
+
+  this.onStop(function () {
+    PlayersList.remove({username:username, roomId:roomId});
   });
 });
