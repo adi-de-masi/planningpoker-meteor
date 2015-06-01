e86c1f6b1ffbfe0b5f5a31b91757b5fb9f5ae0f5
Raffael Schmid <ras@panter.ch>
2015-06-01 17:30:36 +0200

    remove unused players template

--- client/helpers.js
+++ client/helpers.js
@@ -1,17 +1,4 @@
-var usernameEntered = function () {
-   return Session.get('username') !== undefined;
-};
-Template.registerHelper('usernameEntered', function () {
-    return usernameEntered() ? 'invisible' : 'visible';
-});
-Template.registerHelper('voteVisible', function () {
-    return usernameEntered() ? 'visible' : 'invisible';
-});
-Template.registerHelper('currentUser', function() {
+Template.registerHelper('currentUser', function () {
   return Template.currentData().username;
 });
-Template.registerHelper('isChosen', function(username){
-  var username = Template.currentData().username,
-  roomId = Template.currentData().roomId;
-  return VotesList.findOne({'roomId': roomId, 'username': username}) !== undefined;
-});
+
